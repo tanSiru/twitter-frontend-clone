6bb41f26cb2e5357d0a0ece80296ce8efaedd17a
SIRU TAN <siru0915@gmail.com>
2022-12-28 09:24:15 -0500

    Setup sections and downloaded all images

--- react/twitter-clone/src/App.tsx
+++ react/twitter-clone/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="background flex flex-row w-screen">
+      <div className="flex-none w-2/6 h-screen text-rose-500">
+        hello
+      </div>
+      <div className="flex-none w-2/6 h-screen text-rose-500">
+        jajasiadiashpdasihd
+        <span className="left-line"/>
+        <span className="right-line"/>
+      </div>
+      <div className="flex-none w-2/6 h-screen text-rose-500">
+        hello3
+      </div>
     </div>
   );
 }
