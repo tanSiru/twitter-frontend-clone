f856227ba9229ff0e0ebea3ba143019ac4df70cb
SIRU TAN <siru0915@gmail.com>
2023-02-23 08:32:38 -0500

    Working on more dynamic tweets.

--- react/twitter-clone/src/components/Tweets/tweets.tsx
+++ react/twitter-clone/src/components/Tweets/tweets.tsx
@@ -1,7 +1,20 @@
 import HomeTweetBox from "../HomeTweetBox/HomeTweetBox";
 import TweetBox from '../TweetBox/TweetBox'
 
-export default function tweets() {
+var randomSentence = require('random-sentence');
+
+const tweets:any = []
+
+for(let i = 0;i<41;i++){
+  let tweet = {
+    "user":randomSentence({words: 1}),
+    "tweets":randomSentence({min: 4, max: 9})
+  }
+  tweets.push(tweet)
+}
+
+export default function Tweets() {
+  console.log(tweets)
   return (
     <div>
       <HomeTweetBox/>
@@ -43,4 +56,5 @@ export default function tweets() {
 
     </div>
   )
+
 }
