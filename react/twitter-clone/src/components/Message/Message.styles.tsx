c1c70c7dc33fba93fb827896e1cadd0fe1dda62b
SIRU TAN <siru0915@gmail.com>
2023-03-05 11:10:58 -0500

    MessageData(generating msg and where it goes) structure done. Working on sending msgs next.

--- react/twitter-clone/src/components/Message/Message.styles.tsx
+++ react/twitter-clone/src/components/Message/Message.styles.tsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
+type Props = {
+    check:boolean;
+}
+
+type Type = {
+    check:string;
+}
+
 export const Wrapper = styled.div`
     width:100%;
-    height:100%;
+    height:1000px;
     color:white;
 `;
 
@@ -24,12 +32,57 @@ export const Info = styled.div`
         :hover{
             background-color:grey;
         }
-
+    .bio{
+        font-size:20px;
+        color:"white";
+        margin-top:10px;
+        margin-bottom:10px;
+        font-weight:bold;
+    }
+    border-bottom:2px solid grey;
 `
+
+export const Text = styled.div<Props>`
+    font-size:20px;
+    color:${props=>props.check ? "white" : "darkgrey"};
+    font-weight:${props=>props.check ? "bold" : ""};
+`;
 
 export const Pfp = styled.img`  
     clip-path: circle();
-    height:80px;
-    width:80px;
+    height:65px;
+    width:65px;
+`;
 
+export const Messaging = styled.div`
+    display:flex;
+    flex-direction: column;
+    width:450px;
+    height:450px;
+    /* :hover{
+        background-color: grey;
+    } */
+    overflow:auto;
+`;
+
+export const Msg = styled.div<Type>`
+    width:250px;
+    min-height:45px;
+    background-color: #43b4f6;
+    border-radius: 50px;
+    margin-left: ${props=> props.check==="self" ? "195px" : "5px"};
+    margin-top:10px;
+    word-wrap: break-word;
+    justify-items: flex-start;
+    align-items: center;
+    padding-left: 5px;
+`;
+
+export const Send = styled.div`
+    width:450px;
+    height:60px;
+    display: flex;
+    flex-direction:row;
+    border-radius:15px;
+    background-color: red;
 `;
--- react/twitter-clone/src/components/Message/Message.tsx
+++ react/twitter-clone/src/components/Message/Message.tsx
@@ -1,14 +1,51 @@
-import {Wrapper,  Display, Info, Pfp} from './Message.styles'
+import {Wrapper,  Display, Info, Pfp, Text, Messaging, Msg,Send} from './Message.styles'
+
+/*
+Msg data structure:
+Main array/object holding the messages->each object within the main arrray/object will contain the sender + msg
+*/
+
+// type MsgProps={
+//   msg:string;
+//   person:"self" | "other";
+// }
+
+type MsgProps={
+  msg:string;
+  person:string;
+}
+
+var MessageData = [
+  {"phos":[
+    {"msg":"test message","person":"self"},
+    {"msg":"test message","person":"other"},
+  ]}
+]
 
 export default function Message() {
   return (
     <Wrapper>
       <Display>
+
+
         <Info>
           <Pfp src={require('../images/test.jpeg')}/>
-          
+          <Text check={true}>bruh moment</Text>
+          <Text check={false}>test moment</Text>
+          <div className="bio">FML</div>
+          <Text check={false}>March 2, 2023</Text>
+          <Text check={false}>ajsidjaisdas</Text>
         </Info>
+
+        <Messaging>
+          {MessageData[0]["phos"].map((msg:MsgProps)=><Msg check={msg["person"]}>{msg["msg"]}</Msg>)}
+        </Messaging>
+
+        <Send>
+          
+        </Send>
       </Display>
+      
     </Wrapper>
   )
 }
